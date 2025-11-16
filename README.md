7b6468f73e9c258c3e9023816e87d57b4f121f8c
Vitor <backend-obl@modalgr.io>
2025-11-15 21:02:42 -0300

    atualizando insercao e consulta no banco

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Prova-Pratica-DevOps-RNP
 Repositório destinado para a prova prática do processo de DevOps RNP.
+
+Para utlização é necessário ter instalado o Docker e o Docker Compose.
+
+# Comando para executar:
+
+Na raiz do projeto, execute: docker-compose up --build -d
--- ping-agent/ping_agent.py
+++ ping-agent/ping_agent.py
@@ -1,4 +1,6 @@
 from prometheus_client import start_http_server, Gauge
+import psycopg2
+import os
 from ping3 import ping
 import time
 
@@ -30,8 +32,18 @@ def measure_once(target):
             times.append(t)
         time.sleep(0.2)
     loss = 100.0 * (attempts - success) / attempts
-    avg = sum(times)/len(times) if times else 0.0
+    avg = sum(times) / len(times) if times else 0.0
     return avg, loss
+
+
+# Conexão com banco
+conn = psycopg2.connect(
+    host=os.getenv('DB_HOST'),
+    user=os.getenv('DB_USER'),
+    password=os.getenv('DB_PASSWORD'),
+    dbname=os.getenv('DB_NAME')
+)
+cur = conn.cursor()
 
 
 if __name__ == '__main__':
@@ -39,6 +51,13 @@ if __name__ == '__main__':
     while True:
         for target in TARGETS:
             avg, loss = measure_once(target)
+            # salva no banco
+            cur.execute(
+                "INSERT INTO ping_metrics (target, rtt, loss) VALUES (%s, %s, %s)",
+                (target, avg, loss),
+            )
+            conn.commit()
+
             rtt_g.labels(target=target).set(avg)
             loss_g.labels(target=target).set(loss)
         time.sleep(INTERVAL)
